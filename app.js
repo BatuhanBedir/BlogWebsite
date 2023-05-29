fd8324647f4ec50f8dad129ca86d1556b9fba977
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-05-29 15:01:43 +0300

    edited

--- app.js
+++ app.js
@@ -80,27 +80,6 @@ app.post("/compose", function(req,res){
   res.redirect("/");
 });
 
-
-
-// app.get("/posts/:postName",function(req,res){
-//   const requestedTitle = _.lowerCase(req.params.postName);
-
-//   posts.forEach(function(post){
-//     const storedTitle = _.lowerCase(post.title);
-
-//     if (storedTitle===requestedTitle) {
-//       res.render("post",{
-//         title: post.title,
-//         content: post.content
-//       });
-//     }
-//   });
-
-// });
-
-
-
-
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
