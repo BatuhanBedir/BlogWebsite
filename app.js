55d941b2a062d8a27a136a2576a90a98ced9f01e
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-05-29 14:55:00 +0300

    connected to db

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
+
+const url = 'mongodb://localhost:27017/blogDB';
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  family: 4
+};
+mongoose.connect(url, options)
+  .then(()=>{console.log("MongoDb connected");})
+  .catch((err) => console.log(err));
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,14 +25,39 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const postSchema = {
+  title: String,
+  content: String
+}
+
+const Post = mongoose.model("Post",postSchema)
+
 let posts = [];
 
-app.get("/",function(req,res){
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
-  });
+app.get("/", async function(req, res) {
+  try {
+    const posts = await Post.find({});
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
+
+app.get("/posts/:postId", async function(req, res) {
+    const requestedPostId = req.params.postId;
+    const post = await Post.findOne({ _id: requestedPostId });
+    if (!post) {
+      return res.status(404).send("Post not found");
+    }
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+});
+
 
 app.get("/about",function(req,res){
   res.render("about", {aboutContent: aboutContent});
@@ -38,30 +72,31 @@ app.get("/compose",function(req,res){
 });
 
 app.post("/compose", function(req,res){
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
-  }
-  posts.push(post);
-
+  })
+  post.save();
   res.redirect("/");
 });
 
-app.get("/posts/:postName",function(req,res){
-  const requestedTitle = _.lowerCase(req.params.postName);
 
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
 
-    if (storedTitle===requestedTitle) {
-      res.render("post",{
-        title: post.title,
-        content: post.content
-      });
-    }
-  });
+// app.get("/posts/:postName",function(req,res){
+//   const requestedTitle = _.lowerCase(req.params.postName);
 
-});
+//   posts.forEach(function(post){
+//     const storedTitle = _.lowerCase(post.title);
+
+//     if (storedTitle===requestedTitle) {
+//       res.render("post",{
+//         title: post.title,
+//         content: post.content
+//       });
+//     }
+//   });
+
+// });
 
 
 
